019d014115d5b6294e41f92d05f6daa2238699f0
w-kuhn <wendybrookskuhn@gmail.com>
2019-03-05 13:05:12 -0600

    add seed data Co-authored-by: Sachin Chandy <13168187+sachinmc@users.noreply.github.com> Co-authored-by: Nick Miller 28876520+NickMiller11@users.noreply.github.com

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904222019) do
+ActiveRecord::Schema.define(version: 20190305171839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,27 @@ ActiveRecord::Schema.define(version: 20170904222019) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "cards", force: :cascade do |t|
+    t.string "title"
+    t.bigint "list_id"
+    t.datetime "due_date"
+    t.string "labels", default: [], array: true
+    t.string "description"
+    t.float "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.bigint "board_id"
+    t.float "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_lists_on_board_id"
+  end
+
+  add_foreign_key "cards", "lists"
+  add_foreign_key "lists", "boards"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Card.destroy_all
+List.destroy_all
+Board.destroy_all
+
+board1 = Board.create(title: 'Board 1');
+board2 = Board.create(title: 'Board 2');
+list1 = List.create({title: 'list 1', board: board1})
+card1 = Card.create(title: 'card', list: list1, labels: ['red', 'blue'])

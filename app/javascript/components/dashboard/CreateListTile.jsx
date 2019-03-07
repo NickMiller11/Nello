4b63129448d3cd0889c1ce0ae892e7d9694447b1
w-kuhn <w-kuhn@users.noreply.github.com>
2019-03-07 13:56:53 -0600

    toggle add list form Co-authored-by: Sachin Chandy 13168187+sachinmc@users.noreply.github.com Co-authored-by: Nick Miller 28876520+NickMiller11@users.noreply.github.com

--- app/javascript/components/dashboard/CreateListTile.jsx
+++ app/javascript/components/dashboard/CreateListTile.jsx
@@ -4,11 +4,29 @@ import PropTypes from 'prop-types';
 
 class CreateListTile extends React.Component {
   render() {
+  	const classes = ['new-list'];
+
+  	if (this.props.formOpen) {
+  		classes.push('selected');
+  	}
+
     return (
-      <div id="new-list" className="new-list"><span>Add a list...</span>
-          <input type="text" placeholder="Add a list..." />
+      <div id="new-list" className={classes.join(' ')} onClick={this.props.onOpenClick}>
+      		<span>Add a list...</span>
+          <input type="text" 
+          			 placeholder="Add a list..."
+          			 value={this.props.title}
+            	 	 onChange={this.props.onTitleChange}
+          			 />
           <div>
-              <input type="submit" className="button" value="Save" /><i className="x-icon icon"></i>
+            <input type="submit" 
+            			 className="button" 
+            			 value="Save"
+            	 		 onClick={this.props.onFormSubmit}
+            			  />
+            <i className="x-icon icon" 
+            	 onClick={this.props.onCloseClick}
+            ></i>
           </div>
       </div>
     )
--- app/javascript/components/dashboard/CreateListTileContainer.jsx
+++ app/javascript/components/dashboard/CreateListTileContainer.jsx
@@ -3,19 +3,55 @@ import { Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import CreateListTile from './CreateListTile';
 
+
 class CreateListTileContainer extends React.Component {
-  let state = {
-    isOpen: false;
+  state = {
+    isOpen: false,
+		title: '',
+  }
+ 
+
+  handleOpenClick = (evt) => {
+  	evt.stopPropagation();
+    this.setState({isOpen: true});
   }
 
-  const handleOnClick = (evt) => {
-    this.setState = Object.assign({}, {isOpen: true});
+  handleCloseClick = (evt) => {
+  	evt.stopPropagation();
+  	this.setState({isOpen: false});
+  }
+
+  handleSubmit = (evt) => {
+  	evt.preventDefault();
+  	evt.stopPropagation();
+
+  	// dispatch list action to store
+  	// list action is create list
+  	// which hits POST api
+  	// request payload includes title and board_id
+  	// updating list actions, updating lists reducer
+
+  	// other thing we need to do:
+  	// add PUT api/lists/id to routes
+  	// handle click on list title: changes p tag to input tag
+		// handles title change
+		// add update action to lists controller
+  }
+
+  handleTitleChange = (evt) => {
+  	this.setState({title: evt.target.value});
   }
 
   render() {
-    return (
+    return (	
       <CreateListTile
-        onClick={this.handleOnClick}/>
+      	formOpen={this.state.isOpen}
+        onOpenClick={this.handleOpenClick}
+        onCloseClick={this.handleCloseClick}
+        onFormSubmit={this.handleSubmit}
+        onTitleChange={this.handleTitleChange}
+        title={this.state.title}
+      />
     )
   };
 }
